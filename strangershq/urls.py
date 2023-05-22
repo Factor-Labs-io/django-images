72d01fd13cdccb40c5d85a51cbc6d3fad47d9c63
varounsvlogs <120419960+varoun-factorlabs@users.noreply.github.com>
2023-05-22 04:02:52 -0400

    Update urls.py new pfp tracking endpoint to test

--- strangershq/urls.py
+++ strangershq/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddUserView, ReturnUserView, DeleteUserView, UpdateUserHometownView, UpdateUserInterestsView, TwitterTrackingView, LeaderboardView, QueryAllView
+from .views import AddUserView, ReturnUserView, DeleteUserView, UpdateUserHometownView, UpdateUserInterestsView, TwitterTrackingView, LeaderboardView, QueryAllView, ConsumeAPI
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -27,6 +27,7 @@ urlpatterns = [
     path('leaderboardfetch', LeaderboardView.as_view(), name='fetch_leaderboard'),
     path('pfptracking', TwitterTrackingView.as_view(), name='twitter_tracking'),
     path('queryall/', QueryAllView.as_view(), name='get_user_info'),
+    path('consume-api/<str:token_id>/<str:handle>/', ConsumeAPI.as_view(), name='consume-api'),
     # path('', views.home, name='home'),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
